c2c36c8f79d0a58ce6470561c46d714d08a8cf59
cristopher <cristopher>
2023-10-19 18:25:20 -0500

    start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListaProductosComponent } from './component/lista-productos/lista-productos.component';
+import { FormularioProductoComponent } from './component/formulario-producto/formulario-producto.component';
+import { MenuContextualComponent } from './component/menu-contextual/menu-contextual.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaProductosComponent,
+    FormularioProductoComponent,
+    MenuContextualComponent
   ],
   imports: [
     BrowserModule,
